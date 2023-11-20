1ee02b742d1404be0efc58c5167a9c61089fea21
paintdev <the.paint.developer@gmail.com>
2023-11-20 11:19:01 +0200

    basic functioning long-term memory

--- request.py
+++ request.py
@@ -1,33 +1,47 @@
 import requests
 import json
+from os import path
 
 model = "mistral"
-question = "How are you?"
-
 url = "http://localhost:11434/api/generate"
+memory: list = []
 
-data = {"model": f"{model}", "prompt": f"{question}", "stream": False}
+with open("memory.txt","r") as file:
+    file_content = file.read()
+    if path.getsize("memory.txt"):
+        memory = list(map(int, file_content.split(',')))
 
-# Convert the data to JSON format
-payload = json.dumps(data)
+while True:
 
-# Set the headers for the POST request
-headers = {"Content-Type": "application/json"}
+    question = input("Ask: ")
+    
+    data = {"model": f"{model}", "prompt": f"{question}", "stream": False, "context": memory}
 
-# Make the POST request
-response = requests.post(url, data=payload, headers=headers)
+    # Convert the data to JSON format
+    payload = json.dumps(data)
+
+    # Set the headers for the POST request
+    headers = {"Content-Type": "application/json"}
+
+    # Make the POST request
+    response = requests.post(url, data=payload, headers=headers)
 
 
-# Check the response status code and content
-if response.status_code == 200:
-    print("Request successful")
+    # Check the response status code and content
+    if response.status_code == 200:
+        print("Request successful")
 
-    # Convert JSON to dictionary
-    response = response.json()
+        # Convert JSON to dictionary
+        response = response.json()
 
-    # print(response.text)
-    print("Response:")
-    print(response.get("response"))
-    print(response.get("total_duration") / 10**9)
-else:
-    print("Request failed with status code:", response.status_code)
+        # print(response.text)
+        print("Response:")
+        print(response.get("response"))
+        print(response.get("total_duration") / 10**9)
+        memory = response.get("context")
+        with open("memory.txt","w") as file:
+            file.write('')
+            file.write(','.join(map(str, memory)))
+        
+    else:
+        print("Request failed with status code:", response.status_code)
